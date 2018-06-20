37face9529e1817d6e7a8c0bc1b30b620794bd56
Fabian Michel <fabian.michel@tum.de>
2018-06-20 22:00:01 +0200

    Added the Graph class
    The Graph class contains a strongly connected component algorithm,
    other algorithms will be added as needed.

---
+++ Graph.js
@@ -0,0 +1,232 @@
+
+/**
+ * A simple edge with one destination node (source node must be inferred from the adjacency list)
+ */
+class E {
+  /**
+   * Construct a new simple edge
+   * @param {number} v the destination node
+   */
+  constructor(v) {
+    /**
+     * @type {number} the destination node of this edge
+     */
+    this.v = v;
+  }
+}
+
+/**
+ * A weighted edge with one destination node (source node must be inferred from the adjacency list)
+ */
+class WE {
+  /**
+   * Construct a new weighted edge
+   * @param {number} v the destination node
+   * @param {number} w the weight
+   */
+  constructor(v, w) {
+    /**
+     * @type {number} the destination node of this edge
+     */
+    this.v = v;
+    /**
+     * @type {number} the weight of this edge
+     */
+    this.w = w;
+  }
+}
+
+/**
+ * A directed graph (an undirected graph can be represented by two directed edges for every undirected
+ * edge)
+ */
+class Graph {
+  /**
+   * Create a new graph
+   * @param {number} n the number of nodes
+   */
+  constructor(n) {
+    /**
+     * @type {number} the number of nodes in this graph
+     */
+    this.n = n;
+    this.weighted = false;
+    this.adjList = [];
+    this.adjListRev = [];
+    this.nodeMap = new Map();
+    for (let i=0; i<this.n; i++) {
+      this.adjList[i] = [];
+      this.adjListRev[i] = [];
+    }
+  }
+
+  /**
+   * Converts this graph into a graph with the strongly connected components of this graph as nodes.
+   * The nodes of the new graph will be associated with an array containing the nodes that were part
+   * of the strongly connected component in the original graph.
+   * @return {Graph} the new (acyclic) graph
+   */
+  getSCCGraph() {
+    const dfs_low = [], dfs_num = [], visited = [], UNVISITED = -1, EXPLORED = 0, VISITED = 1,
+      inSCC = [];
+    let dfsNumberCounter = 0;
+    for (let i=0; i<this.n; i++) {
+      dfs_low[i] = UNVISITED; dfs_num[i] = UNVISITED; visited[i] = UNVISITED;
+      inSCC[i] = -1;
+    }
+    const s = new Queue();
+    const cEdgesQ = new Queue();
+    const nNodes = [];
+    const nEdges = [];
+
+    const tarjanSCC = u => {
+      const cEdges = new Set();
+      dfs_low[u] = dfs_num[u] = dfsNumberCounter++;
+      s.pushBack(u); cEdgesQ.pushBack(cEdges);
+      visited[u] = EXPLORED;
+      for (let j=0; j<this.adjList[u].length; j++) {
+        const v = this.adjList[u][j].v;
+        if (dfs_num[v] == UNVISITED) {
+          const conTo = tarjanSCC(v);
+          if (conTo != -1) cEdges.add(conTo);
+        } else {
+          if (inSCC[v] != -1) cEdges.add(inSCC[v]);
+        }
+        if (visited[v] == EXPLORED)
+          dfs_low[u] = Math.min(dfs_low[u], dfs_low[v]);
+      }
+      if (dfs_low[u] == dfs_num[u]) {
+        const scc = [];
+        while (true) {
+          const v = s.popBack(); visited[v] = VISITED; inSCC[v] = nNodes.length;
+          scc.push(this.getNode(v));
+          const eSet = cEdgesQ.popBack();
+          if (u == v) break;
+          for (const eTo of eSet) cEdges.add(eTo);
+        }
+        nNodes.push(scc);
+        for (const eTo of cEdges) nEdges.push([nNodes.length - 1, eTo]);
+        return nNodes.length - 1;
+      }
+      return -1;
+    };
+    
+    for (let i=0; i<this.n; i++)
+      if (dfs_num[i] == UNVISITED)
+        tarjanSCC(i);
+
+    const ret = new Graph(nNodes.length);
+    for (let i=0; i<nEdges.length; i++) ret.insertEdge(nEdges[i][0], nEdges[i][1]);
+    for (let i=0; i<nNodes.length; i++) ret.setNode(i, nNodes[i]);
+
+    return ret;
+  }
+
+  /**
+   * Get a topological sort of this graph's nodes
+   * @return {any[]} an array with the nodes in topological order. If setNode() was used, this array
+   * will contain the associated objects, otherwise it will contain the node's indices. The method
+   * will return null if this is not an acyclic graph.
+   */
+  topSort() {
+    const outgoing = []; let nodesProcessed = 0;
+    const topS = [];
+    const q = new Queue();
+    for (let i=0; i<this.n; i++) {
+      outgoing[i] = this.adjList[i].length;
+      if (outgoing[i] == 0) {
+        nodesProcessed++;
+        q.pushBack(i);
+        topS.push(this.getNode(i));
+      }
+    }
+    while (!q.isEmpty()) {
+      const u = q.popFront();
+      for (let i=0; i<this.adjListRev[u].length; i++) {
+        const v = this.adjListRev[u][i].v;
+        outgoing[v]--;
+        if (outgoing[v] == 0) {
+          nodesProcessed++;
+          q.pushBack(v);
+          topS.push(this.getNode(v));
+        }
+      }
+    }
+    if (nodesProcessed == this.n) {
+      return topS.reverse();
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Check whether this graph is acyclic (only works for directed graphs)
+   * @return {boolean} true if the graph is acyclic
+   */
+  isAcyclic() {
+    return this.topSort() !== null;
+  }
+
+  /**
+   * Insert a new edge into this graph (you should consistently use the weight or not use it, don't mix
+   * weighted and unweighted edges)
+   * @param {number} u the source node
+   * @param {number} v the destination node
+   * @param {number} [w] the weight of the edge
+   */
+  insertEdge(u, v, w) {
+    if (typeof w === 'number') {
+      this.adjList[u].push(new WE(v, w));
+      this.adjListRev[v].push(new WE(u, w));
+      this.weighted = true;
+    } else {
+      this.adjList[u].push(new E(v));
+      this.adjListRev[v].push(new E(u));
+    }
+  }
+
+  /**
+   * Insert a new undirected edge into this graph (you should consistently use the weight or not use it,
+   * don't mix weighted and unweighted edges)
+   * @param {number} u the source node
+   * @param {number} v the destination node
+   * @param {number} [w] the weight of the edge
+   */
+  insertUndirectedEdge(u, v, w) {
+    this.insertEdge(u, v, w);
+    this.insertEdge(v, u, w);
+  }
+
+  /**
+   * If a specific value for the given node was set, this value will be returned. Otherwise, this
+   * method will return its only parameter.
+   * @param {number} i the number of the node
+   * @return {any} the node's value or i (see description above)
+   */
+  getNode(i) {
+    if (this.nodeMap.has(i)) return this.nodeMap.get(i);
+    return i;
+  }
+
+  /**
+   * Associates a node with an object. This can be useful with some graph algorithms that will
+   * return the associated objects instead of the node indices.
+   * @param {number} i the number of the node
+   * @param {any} obj the object the node should be associated with
+   */
+  setNode(i, obj) {
+    this.nodeMap.set(i, obj);
+  }
+
+  /**
+   * Get an array with all nodes in this graph
+   * @return {any[]} the array with the nodes. If setNode() was used, this array will contain the
+   * associated objects. Otherwise, it will contain the node's indices (that is, the array will simply
+   * contain the numbers from 0 to n-1).
+   */
+  getAllNodes() {
+    const ret = [];
+    for (let i=0; i<this.n; i++) ret.push(this.getNode(i));
+    return ret;
+  }
+}
---
+++ Graph.test.js
@@ -0,0 +1,71 @@
+
+function GraphTest() {
+  // sample data
+  const nodes = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'];
+  const edges = [
+    ['a', 'b'], ['a', 'e'], ['b', 'c'], ['c', 'i'], ['d', 'f'], ['e', 'k'], ['f', 'g'],
+    ['g', 'd'], ['g', 'e'], ['g', 'l'], ['h', 'g'], ['h', 'm'], ['h', 'n'], ['i', 'b'],
+    ['i', 'h'], ['j', 'f'], ['k', 'j'], ['k', 'l'], ['l', 'm']
+  ];
+  const nodes2_num = 15;
+  const edges2 = [
+    [0, 10], [1, 6], [1, 13], [2, 12], [3, 2], [3, 9], [3, 11], [4, 3], [5, 6], [6, 0],
+    [7, 0], [7, 1], [8, 13], [9, 4], [10, 14], [11, 7], [11, 8], [12, 3], [13, 5], [13, 11],
+    [14, 6]
+  ];
+
+  // --> Graph
+  const g = new Graph(nodes.length);
+  for (let i=0; i<nodes.length; i++) g.setNode(i, nodes[i]);
+  for (let i=0; i<edges.length; i++)
+    g.insertEdge(nodes.indexOf(edges[i][0]), nodes.indexOf(edges[i][1]));
+  assertStrictEqual(g.n, nodes.length);
+  for (let i=0; i<nodes.length; i++) assertStrictEqual(g.getNode(i), nodes[i]);
+  assertStrictEqual(g.isAcyclic(), false);
+
+  const g2 = g.getSCCGraph();
+  assertStrictEqual(g2.n, 7);
+  assertStrictEqual(g2.isAcyclic(), true);
+  const topSort = g2.topSort();
+  const allNodes = g2.getAllNodes();
+  assertNotStrictEqual(topSort, null);
+  assertStrictEqual(allNodes.length, g2.n);
+  const expAllNodes = [['m'], ['l'], ['j', 'k', 'e', 'f', 'd', 'g'], ['n'], ['h'], ['i', 'c', 'b'], ['a']];
+  const expTopSort = [['a'], ['i', 'c', 'b'], ['h'], ['j', 'k', 'e', 'f', 'd', 'g'], ['l'], ['n'], ['m']];
+  for (let i=0; i<expAllNodes.length; i++)
+    assertArrayCmp(expAllNodes[i].sort(), allNodes[i].sort(), assertStrictEqual);
+  for (let i=0; i<expTopSort.length; i++)
+    assertArrayCmp(expTopSort[i].sort(), topSort[i].sort(), assertStrictEqual);
+  const adjListEntries = [[], [0], [1], [], [0, 2, 3], [4], [2, 5]];
+  for (let i=0; i<7; i++) {
+    assertStrictEqual(g2.adjList[i].length, adjListEntries[i].length);
+    for (let k=0; k<g2.adjList[i].length; k++) {
+      const v = g2.adjList[i][k].v; const idx = adjListEntries[i].indexOf(v);
+      assertNotStrictEqual(idx, -1); adjListEntries[i][idx] = -1;
+    }
+  }
+
+  const g3 = new Graph(nodes2_num);
+  for (let i=0; i<edges2.length; i++)
+    g3.insertEdge(edges2[i][0], edges2[i][1]);
+  assertStrictEqual(g3.n, nodes2_num);
+  for (let i=0; i<nodes2_num; i++) assertStrictEqual(g3.getNode(i), i);
+  assertStrictEqual(g3.isAcyclic(), false);
+
+  const g4 = g3.getSCCGraph();
+  assertStrictEqual(g4.n, 4);
+  assertStrictEqual(g4.isAcyclic(), true);
+  const topSort2 = g4.topSort();
+  assertNotStrictEqual(topSort2, null);
+  const expTopSort2 = [[2, 3, 4, 9, 12], [1, 7, 8, 11, 13], [5], [0, 6, 10, 14]];
+  for (let i=0; i<expTopSort2.length; i++)
+    assertArrayCmp(expTopSort2[i].sort(), topSort2[i].sort(), assertStrictEqual);
+  const adjListEntries2 = [[], [0], [0, 1], [2]];
+  for (let i=0; i<4; i++) {
+    assertStrictEqual(g4.adjList[i].length, adjListEntries2[i].length);
+    for (let k=0; k<g4.adjList[i].length; k++) {
+      const v = g4.adjList[i][k].v; const idx = adjListEntries2[i].indexOf(v);
+      assertNotStrictEqual(idx, -1); adjListEntries2[i][idx] = -1;
+    }
+  }
+}
