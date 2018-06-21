34d9edf0e4c843d2bd127b83a2119b3440160aea
Fabian Michel <fabian.michel@tum.de>
2018-06-22 00:17:48 +0200

    Added automated documentation generation

---
+++ docs/README.md
@@ -0,0 +1,11 @@
+# Common data structures for JavaScript
+
+This is an implementation of some of the most common data structures in JavaScript.
+
+The provided data structures are:
+- A tree set (an ordered set)
+- A binary and a binomial heap, to be used as a priority queue
+- A hash set (note that JavaScript already provides the Set class)
+- A linked list (as well as a doubly linked list)
+- A queue (based on an array)
+- A graph (currently, the graph only supports topological sort and the SCC algorithm)
---
+++ docs/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const pr = require('child_process');
+
+let files = [], file_contents = [], classes = [];
+fs.readdirSync('../').forEach(file => {
+  files.push(file);
+});
+
+files = files.filter(name => name.search(/\.js$/) != -1);
+
+for (let i=0; i<files.length; i++) {
+  let regex = /\r?\nexport class ([A-Za-z_0-9]+)/g;
+  file_contents[i] = fs.readFileSync('../' + files[i], 'utf8');
+  file_contents[i] = file_contents[i].replace(/(\r?\n)class/g, '$1export class');
+  let result;
+  while ((result = regex.exec(file_contents[i])) !== null) {
+    classes.push(result[1]);
+  }
+  fs.writeFileSync('js/' + files[i], file_contents[i], 'utf8');
+}
+
+console.log('found ' + files.length + ' files: ' + files.join(', '));
+console.log('found ' + classes.length + ' classes: ' + classes.join(', '));
+console.log('running esdoc...');
+
+pr.execSync('./node_modules/.bin/esdoc', {stdio:[0,1,2]});
